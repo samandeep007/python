a0cbee3a63a621eec956128db44f8d31f4e5a9f5
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 18:06:39 -0600

    Add Strings.py file

---
+++ 01_basics/Strings.py
@@ -0,0 +1,10 @@
+# >>> # '' or " " or """ """ --> String literals in Python are enclosed in either single quotes, double quotes, or triple quotes. Triple quotes can span multiple lines and are useful when you want to include a lot of text
+# >>> chai = "Lemon Chai"
+# >>> print(chai)
+# Lemon Chai
+
+
+# >>> chai = "Masala chai"
+# >>> firstChar = chai[0]
+# >>> print(firstChar)  
+# M
