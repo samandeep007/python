2d5dfe26f7901e1118baa69240a02ef08b2e0672
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 19:12:15 -0600

    Add basic list operations and examples to Lists.py

---
+++ 01_basics/Lists.py
@@ -0,0 +1,9 @@
+# >>> tea_varieties = ['Black', 'Green', 'Oolong', 'White']
+# >>> print(tea_varieties)
+# ['Black', 'Green', 'Oolong', 'White']
+
+# >>> print(tea_varieties[0])
+# Black
+# >>> print(tea_varieties[-1])
+# White
+
