1a63d4e6e488c5886eed81ffa8f44c419f10b264
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 20:21:02 -0600

    feat: Add dictionary access examples to Dictionaries.py

---
+++ 01_basics/Dictionaries.py
@@ -0,0 +1,15 @@
+# >>> chai_types = {"Masala": "Spicy", "Ginger": "Zesty", "Green": "Mild"}
+
+# Ways to access elements:
+# >>> chai_types["Masala"]
+# 'Spicy'
+# >>> chai_types.get("Masala")
+# 'Spicy'
+
+
+# >>> chai_types.get("Lemon")
+# >>> chai_types["Lemon"]
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# KeyError: 'Lemon'
+
