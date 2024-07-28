acffbaed3a32454ff5663a7f6af98e858d816ff7
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 23:29:33 -0600

    Refactor grading logic in 02_conditionals/03_solution.py

---
+++ 02_conditionals/03_solution.py
@@ -0,0 +1,16 @@
+# Problem: Assign a letter grade based on a student's score: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
+
+def getGrade(score):
+    if score < 60:
+        return 'F'
+    elif score < 70:
+        return 'D'
+    elif score < 80:
+        return 'C'
+    elif score < 90:
+        return 'B'
+    else:
+        return 'A'
+    
+score = int(input("Enter your score: "))
+print("Your grade is " + str(getGrade(score)))
