ba1b5fad3cc9d08d78468b507f405941924842bd
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-28 00:56:17 -0600

    refactor: Improve prime number checking function in 03_loops/08_solution.py

---
+++ 03_loops/08_solution.py
@@ -0,0 +1,16 @@
+# 8. Prime Number Checker
+# </summary>
+# Problem: Check if a number is prime.
+
+def checkPrime(num):
+    if num <= 1:
+        return False  # Numbers less than or equal to 1 are not prime
+    if num == 2:
+            return True
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+print("The number is " + ("" if checkPrime(num) == True else "not ") + "prime")
