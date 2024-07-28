35e61de77124475f16a23d83a0d15da9f83af1e6
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 19:18:01 -0600

    Update Lists.py with correct list slicing and assignment syntax

--- 01_basics/Lists.py
+++ 01_basics/Lists.py
@@ -18,7 +18,10 @@
 
 # >>> tea_varieties[1:2]
 # ['Green']
-# >>> tea_varieties[1:2] = "Lemon"
+# >>> tea_varieties[1:2] = "Lemon" --> Lemon will be treated as a list
 # >>> print(tea_varieties)
 # ['Black', 'L', 'e', 'm', 'o', 'n', 'Oolong', 'herbal']
 
+# >>> tea_varieties[1:2] = ["Lemon"]
+# >>> print(tea_varieties)
+# ['Black', 'Lemon', 'Oolong', 'White']
