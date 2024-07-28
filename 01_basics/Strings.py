8ea1868a5d0474809d2c0dab6188a289e0085935
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 18:11:39 -0600

    Update Strings.py with additional examples and explanations

--- 01_basics/Strings.py
+++ 01_basics/Strings.py
@@ -15,11 +15,14 @@
 # >>> slice_chai = chai[0:6] # last index is not included
 # >>> print(slice_chai)
 # Masala
+
 # >>> num_list = "0123456789"
 # >>> num_list[:] 
 # '0123456789'
-# >>> num_list[3:]
+# >>> num_list[3:] 
 # '3456789'
-# >>> num_list[:7]
+# >>> num_list[:7] # first 7 characters
 # '0123456'
-# >>>
+# >>> num_list[0:7:2] # Step size of 2
+# '0246'
+
