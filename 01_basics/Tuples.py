ab95cc8e0f53d79c2c66a35521bc1a63d54db693
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 21:29:20 -0600

    Update Tuples.py with tuple access examples and fix typo in comments

---
+++ 01_basics/Tuples.py
@@ -0,0 +1,20 @@
+# >>> tea_types = ("Black", "Green", "Lemon")
+
+# >>> tea_types[0]
+# 'Black'
+# >>> tea_types[-1]
+# 'Lemon'
+# >>> tea_types[:1]
+# ('Black',)
+
+# >>> tea_types[0] = "Oolong" --> Trying to change the value of a tuple is not allowed.
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'tuple' object does not support item assignment
+
+
+# >>> print(len(tea_types))
+# 3
+# >>> print(tea_types[:2])
+# ('Black', 'Green')
+
