df3eb9fb4329ba5f91e22bc9053ee8716264f344
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 23:22:08 -0600

    chore: Refactor movie ticket pricing logic and input handling in 02_conditionals\02_solution.py

--- 02_conditionals/02_solution.py
+++ 02_conditionals/02_solution.py
@@ -1,16 +1,16 @@
 # Problem: Movie tickets are priced based on age: $12 for adults (18 and over), $8 for children. Everyone gets a $2 discount on Wednesday.
-
-def getTicketPrice(age, day):
+import datetime
+def getTicketPrice(age):
+    today = datetime.datetime.today()
     ticketPrice = 0
     if age>=18:
        ticketPrice = 12
     else:
         ticketPrice = 8
-    if(day.lower() == "wednesday"):
+    if(today.weekday() == 3):
         ticketPrice = ticketPrice - 2
     return ticketPrice
 
 
 age = int(input("Enter your age: "))
-day = input("Enter the day today: ")
-print("The ticket costs $" + str(getTicketPrice(age, day)))
+print("The ticket costs $" + str(getTicketPrice(age)))
