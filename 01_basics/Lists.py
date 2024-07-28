0c8f94c94da3694149187ab5d4081072b4ef860c
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-07-27 19:15:51 -0600

    Update Lists.py with list slicing and assignment examples

--- 01_basics/Lists.py
+++ 01_basics/Lists.py
@@ -6,4 +6,19 @@
 # Black
 # >>> print(tea_varieties[-1])
 # White
+# >>> print(tea_varieties[0:2])
+# ['Black', 'Green']
+# >>> print(tea_varieties[2:])
+# ['Oolong', 'White']
+
+# >>> tea_varieties[3] = 'herbal'
+# >>> print(tea_varieties)
+# ['Black', 'Green', 'Oolong', 'herbal']
+
+
+# >>> tea_varieties[1:2]
+# ['Green']
+# >>> tea_varieties[1:2] = "Lemon"
+# >>> print(tea_varieties)
+# ['Black', 'L', 'e', 'm', 'o', 'n', 'Oolong', 'herbal']
 
